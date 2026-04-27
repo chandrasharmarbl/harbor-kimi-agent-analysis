d024782e495bed92d8808b36c130f9d97b7f272c
Chandra Sharma <chandrakant@incubyte.co>
2026-04-27 11:23:51 +0530

    feat: add automated ansible installation to solve script

--- nestjs-docker-setup/solution/solve.sh
+++ nestjs-docker-setup/solution/solve.sh
@@ -5,6 +5,12 @@ set -e
 # docker compose to work correctly.
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $SCRIPT_DIR
+# Install ansible if not installed.
+if ! command -v ansible-playbook &> /dev/null; then
+    echo "Ansible not installed. Installing it"
+    apt-get update -qq
+    apt-get install -y -qq ansible > /dev/null
+fi
 
 ansible-playbook ansible/install-docker.yml
 
